621e6dc90ae1cb7f6e9517d878aaa4801a9fb020
ly653 sa <desthekkinfng51@hotmail.com>
2024-08-19 13:26:12 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("lyh","lỵ")
+        ERC721("lu","hu")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
